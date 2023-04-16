5f324c4c4a0d61b9819022014b815876bc919861
vladimir ostanin <bob.ost@mail.ru>
2023-04-16 23:12:49 +0600

    t

--- homework.py
+++ homework.py
@@ -164,7 +164,7 @@ def read_package(workout_type: str, data: list) -> Training:
 def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
-    print(info.get_message())
+    print(str(info.get_message()))
 
 
 if __name__ == "__main__":
